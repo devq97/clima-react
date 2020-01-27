c890688aaa5fd017e39afbd370ab9d857715dbe6
David Esteban Vargas Quevedo <david.vargas@decameron.com>
2020-01-27 07:57:24 -0500

    Se consulta la API, se pasa el stete al formulario.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 
 function App() {
+
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect( () => {
+    const consultarAPI = async () => {
+
+      if(consultar) {
+
+        const appId = '0977c423bdf1c2574a3f79fe2e8be35c';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        console.log(resultado);
+      }
+    };
+    consultarAPI();
+  }, [consultar, ciudad, pais]);
+
   return (
     <Fragment>
       <Header
@@ -14,7 +39,9 @@ function App() {
           <div className="row">
             <div className="col m6 s12">
               <Formulario
-
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
               />
             </div>
             <div className="col m6 s12">
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 
-const Formulario = () => {
-
-  const [busqueda, guardarBusqueda] = useState({
-    ciudad: '',
-    pais: ''
-  });
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
 
   const [error, guardarError] = useState(false);
 
@@ -27,6 +22,8 @@ const Formulario = () => {
     }
 
     guardarError(false);
+
+    guardarConsultar(true);
   }
 
   return (
